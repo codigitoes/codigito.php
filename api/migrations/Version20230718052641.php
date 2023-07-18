c7fb23a97c2b1822a11f39058553e128f397ada4
Marco Antonio <mrubio@karecovering.com>
2023-07-18 07:31:12 +0200

    - primera migracion para tener la estructura y relaciones facil de poner, ahora vamos a poner los fixture

---
+++ api/migrations/Version20230718052641.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230718052641 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE blogcontents CHANGE image image VARCHAR(255) DEFAULT NULL, CHANGE youtube youtube VARCHAR(255) DEFAULT NULL, CHANGE created created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
+        $this->addSql('ALTER TABLE blogposts CHANGE created created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
+        $this->addSql('ALTER TABLE credentials CHANGE roles roles JSON NOT NULL, CHANGE created created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, CHANGE updated updated DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
+        $this->addSql('ALTER TABLE fortunes CHANGE created created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
+        $this->addSql('ALTER TABLE mailings CHANGE created created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
+        $this->addSql('ALTER TABLE tags CHANGE created created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE tags CHANGE created created DATETIME DEFAULT \'current_timestamp()\' NOT NULL');
+        $this->addSql('ALTER TABLE blogcontents CHANGE image image VARCHAR(255) DEFAULT \'NULL\', CHANGE youtube youtube VARCHAR(255) DEFAULT \'NULL\', CHANGE created created DATETIME DEFAULT \'current_timestamp()\' NOT NULL');
+        $this->addSql('ALTER TABLE mailings CHANGE created created DATETIME DEFAULT \'current_timestamp()\' NOT NULL');
+        $this->addSql('ALTER TABLE fortunes CHANGE created created DATETIME DEFAULT \'current_timestamp()\' NOT NULL');
+        $this->addSql('ALTER TABLE blogposts CHANGE created created DATETIME DEFAULT \'current_timestamp()\' NOT NULL');
+        $this->addSql('ALTER TABLE credentials CHANGE roles roles LONGTEXT NOT NULL COLLATE `utf8mb4_bin`, CHANGE created created DATETIME DEFAULT \'current_timestamp()\' NOT NULL, CHANGE updated updated DATETIME DEFAULT \'current_timestamp()\' NOT NULL');
+    }
+}
