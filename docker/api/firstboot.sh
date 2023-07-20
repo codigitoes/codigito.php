2ad7991c9d72bd371a757f6eacc64996c5f11ce7
Marco Antonio <mrubio@karecovering.com>
2023-07-20 22:30:46 +0200

    - empezamos a montar infra de red pra despliege de docker en produccion a topeee

--- docker/api/firstboot.sh
+++ docker/api/firstboot.sh
@@ -3,9 +3,9 @@
 git config --global user.email "codigito"
 git config --global user.name "{.''.} ~ Codigo Com Es"
 cd /tmp/ && /usr/local/bin/symfony new codigito  && cd codigito && chown -R 1000:1000 * && find ./ -name .git -type d | xargs rm -fr
-rm -fr /var/www/vhosts/codigito/.* /var/www/vhosts/codigito/*
-mv * .env .gitignore /var/www/vhosts/codigito/
-cd /var/www/vhosts/codigito/
+rm -fr /var/www/vhosts/codigito.es/.* /var/www/vhosts/codigito.es/*
+mv * .env .gitignore /var/www/vhosts/codigito.es/
+cd /var/www/vhosts/codigito.es/
 composer install
 composer require symfony/orm-pack --no-interaction
 composer require --dev phpunit --no-interaction
--- docker/www/firstboot.sh
+++ docker/www/firstboot.sh
@@ -3,7 +3,7 @@
 git config --global user.email "codigito"
 git config --global user.name "{.''.} ~ Codigo Com Es"
 cd /tmp/ && /usr/local/bin/symfony new codigito  --webapp  && cd codigito && chown -R 1000:1000 * && find ./ -name .git -type d | xargs rm -fr
-rm -fr /var/www/vhosts/codigito/.* /var/www/vhosts/codigito/*
-mv * .env .gitignore /var/www/vhosts/codigito/
-cd /var/www/vhosts/codigito/
+rm -fr /var/www/vhosts/codigito.es/.* /var/www/vhosts/codigito.es/*
+mv * .env .gitignore /var/www/vhosts/codigito.es/
+cd /var/www/vhosts/codigito.es/
 composer install
