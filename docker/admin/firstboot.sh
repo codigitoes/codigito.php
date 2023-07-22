27147e3dc950ab687038bb6cdfe92fdbf0be3799
Marco Antonio <codigito.es@gmail.com>
2023-07-22 14:04:59 +0200

    - ponemos promtail en www admin y api para los logs de symfony no los de apache

--- docker/admin/firstboot.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-git config --global user.email "codigito"
-git config --global user.name "{.''.} ~ Codigo Com Es"
-cd /tmp/ && /usr/local/bin/symfony new codigito  --webapp  && cd codigito && chown -R 1000:1000 * && find ./ -name .git -type d | xargs rm -fr
-rm -fr /var/www/vhosts/codigito.es/.* /var/www/vhosts/codigito.es/*
-mv * .env .gitignore /var/www/vhosts/codigito.es/
-cd /var/www/vhosts/codigito.es/
-composer install
--- docker/www/entrypoint.sh
+++ docker/www/entrypoint.sh
@@ -3,4 +3,8 @@
 # Start the run once job.
 echo "markitos say: 'Docker container has been started from entrypoint.sh :)'"
 
+echo "markitos say: 'starting promtail'"
+/usr/local/bin/promtail -config.file /etc/promtail-local-config.yaml & 
+echo "markitos say: 'promtail started'"
+
 apachectl -D FOREGROUND
