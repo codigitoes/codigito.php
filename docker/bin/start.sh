cc691eedf36b3a2ddb0e2fd6f437d424faf5f5d9
markitos <codigito.es@gmail.com>
2023-08-13 04:20:42 +0200

    - sistemas

--- docker/bin/start.sh
+++ docker/bin/start.sh
@@ -15,4 +15,4 @@ if [ $# -eq 0 ]
     exit
 fi
 
-DEPLOY_ENV=$1 docker-compose up --wait -d --build --remove-orphans
+DEPLOY_ENV=$1 docker compose up --wait -d --build --remove-orphans --force-recreate
