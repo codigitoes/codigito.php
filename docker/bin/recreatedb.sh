6611a24af69f697e6d00bf28d435c296dc03b838
Marco Antonio - mArKit0s <codigito.es@gmail.com>
2023-08-21 20:46:35 +0200

    fix dbç

--- docker/bin/recreatedb.sh
+++ docker/bin/recreatedb.sh
@@ -14,6 +14,6 @@ echo ""
 docker exec -it `docker ps | grep codigito.api | head -n1 | awk '{print $1;}'`  php bin/console doctrine:database:drop --force
 docker exec -it `docker ps | grep codigito.api | head -n1 | awk '{print $1;}'`  php bin/console doctrine:database:create
 docker exec -it `docker ps | grep codigito.api | head -n1 | awk '{print $1;}'`  php bin/console doctrine:schema:update --force
-docker exec -it `docker ps | grep codigito.mariadb | head -n1 | awk '{print $1;}'` mysql -u root -proot -h codigito.mariadb -e "$(cat mariadb/fixture.sql)"
+docker exec -it `docker ps | grep codigito.mariadb | head -n1 | awk '{print $1;}'` mysql -u codigito -pcodigito -h codigito.mariadb -e "$(cat mariadb/fixture.sql)"
 echo ""
 echo ""
